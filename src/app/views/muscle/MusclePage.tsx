e2a295f89839b39414cf88f3ceaa54ce269a909f
okoeee <yk27.puxcm@gmail.com>
2022-09-24 13:59:58 +0900

    input page調節

--- src/app/views/muscle/MusclePage.tsx
+++ src/app/views/muscle/MusclePage.tsx
@@ -1,7 +1,26 @@
+import Button from '@suid/material/Button';
+import TextField from '@suid/material/TextField';
 import styles from './MusclePage.module.scss';
 
 const MusclePage = () => {
-  return <div class={styles.host} />;
+  return (
+    <div class={styles.container}>
+      <TextField
+        required
+        label="メニュー"
+        defaultValue="スクワット"
+        class={styles.textField}
+      />
+      <TextField required label="回数" type="number" class={styles.textField} />
+      <TextField
+        required
+        label="セット数"
+        type="number"
+        class={styles.textField}
+      />
+      <Button variant="contained">筋トレ開始！</Button>
+    </div>
+  );
 };
 
 export default MusclePage;
