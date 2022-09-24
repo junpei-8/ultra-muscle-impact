a26cb426d8ecaaebe481660bb578e61de027626d
okoeee <yk27.puxcm@gmail.com>
2022-09-24 16:10:34 +0900

    inputの値をstoreに保存

--- src/app/store/root.ts
+++ src/app/store/root.ts
@@ -1,3 +1,5 @@
 import { createSignal } from 'solid-js';
 
 export const rootActionsState = createSignal(true);
+export const inputNumberOfTimes = createSignal(0);
+export const inputSetCount = createSignal(0);
--- src/app/views/muscle/MusclePage.tsx
+++ src/app/views/muscle/MusclePage.tsx
@@ -1,9 +1,30 @@
 import Button from '@suid/material/Button';
 import TextField from '@suid/material/TextField';
 import { Link } from 'solid-app-router';
+import { JSX } from 'solid-js';
+import { inputNumberOfTimes, inputSetCount } from '../../store/root';
 import styles from './MusclePage.module.scss';
 
 const MusclePage = () => {
+  const [numberOfTimes, setNumberOfTimes] = inputNumberOfTimes;
+  const [setCount, setSetCount] = inputSetCount;
+
+  const updateNumberOfTimes: JSX.EventHandlerUnion<HTMLInputElement, Event> = (
+    e,
+  ) => {
+    setNumberOfTimes(Number((e.target as HTMLInputElement).value));
+  };
+
+  const updateSetCount: JSX.EventHandlerUnion<HTMLInputElement, Event> = (
+    e,
+  ) => {
+    setSetCount(Number((e.target as HTMLInputElement).value));
+  };
+
+  const submitMenu = () => {
+    console.log(numberOfTimes(), setCount());
+  };
+
   return (
     <div class={styles.container}>
       <TextField
@@ -12,16 +33,25 @@ const MusclePage = () => {
         defaultValue="スクワット"
         class={styles.textField}
       />
-      <TextField required label="回数" type="number" class={styles.textField} />
+      <TextField
+        required
+        label="回数"
+        type="number"
+        class={styles.textField}
+        value={numberOfTimes()}
+        onChange={updateNumberOfTimes}
+      />
       <TextField
         required
         label="セット数"
         type="number"
         class={styles.textField}
+        onChange={updateSetCount}
       />
       <Button variant="contained">
         <Link href="/action">筋トレ開始！</Link>
       </Button>
+      <Button onClick={submitMenu}>test</Button>
     </div>
   );
 };
