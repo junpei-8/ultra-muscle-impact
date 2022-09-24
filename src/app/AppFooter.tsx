4af651905ef9f28c17a5da208d39f440131570fe
okoeee <yk27.puxcm@gmail.com>
2022-09-24 12:09:38 +0900

    bottomnavにiconを追加

--- src/app/AppFooter.tsx
+++ src/app/AppFooter.tsx
@@ -1,3 +1,6 @@
+import LibraryBooksIcon from '@suid/icons-material/LibraryBooks';
+import PersonIcon from '@suid/icons-material/Person';
+import SettingsIcon from '@suid/icons-material/Settings';
 import Button from '@suid/material/Button';
 import { Link } from 'solid-app-router';
 import styles from './AppFooter.module.scss';
@@ -7,12 +10,26 @@ const AppFooter = () => {
     <footer class={styles.host}>
       <div class={styles.navItem}>
         <Link href="/">
-          <Button>マッスル</Button>
+          <div>
+            <PersonIcon />
+            <Button>マッスル</Button>
+          </div>
         </Link>
       </div>
       <div class={styles.navItem}>
         <Link href="/muscle/">
-          <Button>図鑑</Button>
+          <div>
+            <LibraryBooksIcon />
+            <Button>図鑑</Button>
+          </div>
+        </Link>
+      </div>
+      <div class={styles.navItem}>
+        <Link href="/muscle/">
+          <div>
+            <SettingsIcon />
+            <Button>設定</Button>
+          </div>
         </Link>
       </div>
     </footer>
