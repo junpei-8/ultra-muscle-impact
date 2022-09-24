9062249ea1e8129e9cc7d81c4cf1314ddc3dcd86
okoeee <yk27.puxcm@gmail.com>
2022-09-24 11:46:58 +0900

    create header and footer

--- src/app/AppFooter.tsx
+++ src/app/AppFooter.tsx
@@ -1,5 +1,22 @@
+import Button from '@suid/material/Button';
+import { Link } from 'solid-app-router';
+import styles from './AppFooter.module.scss';
+
 const AppFooter = () => {
-  return <footer>{}</footer>;
+  return (
+    <footer class={styles.host}>
+      <div class={styles.navItem}>
+        <Link href="/">
+          <Button>マッスル</Button>
+        </Link>
+      </div>
+      <div class={styles.navItem}>
+        <Link href="/muscle/">
+          <Button>図鑑</Button>
+        </Link>
+      </div>
+    </footer>
+  );
 };
 
 export default AppFooter;
--- src/app/AppHeader.tsx
+++ src/app/AppHeader.tsx
@@ -1,9 +1,22 @@
+import MenuIcon from '@suid/icons-material/Menu';
 import AppBar from '@suid/material/AppBar';
+import Box from '@suid/material/Box';
+import IconButton from '@suid/material/IconButton';
+import Toolbar from '@suid/material/Toolbar';
+import Typography from '@suid/material/Typography';
 
 const AppHeader = () => {
   return (
     <header>
-      <AppBar>App Bar</AppBar>
+      <Box sx={{ flexGrow: 1 }}>
+        <AppBar position="static">
+          <Toolbar>
+            <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+              Muscle Impact
+            </Typography>
+          </Toolbar>
+        </AppBar>
+      </Box>
     </header>
   );
 };
