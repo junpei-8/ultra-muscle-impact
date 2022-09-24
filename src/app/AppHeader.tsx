30c9eb13db821f4780b383fc4a4a1eed21386f24
junpei-8 <miurajunpeis2@gmail.com>
2022-09-24 10:20:41 +0900

    update: github actions test

--- src/app/AppHeader.tsx
+++ src/app/AppHeader.tsx
@@ -3,7 +3,7 @@ import AppBar from '@suid/material/AppBar';
 const AppHeader = () => {
   return (
     <header>
-      <AppBar>App Bar</AppBar>
+      <AppBar>App Bar（Github Action Test）</AppBar>
     </header>
   );
 };
