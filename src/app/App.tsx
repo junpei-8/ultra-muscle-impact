3c852c3b95a4b2763e4dfa707128a129bb161d7c
junpei-8 <junpei.v8@gmail.com>
2023-12-07 15:45:07 +0900

    update: Push しそこねてたリファクタリングのデプロイ！

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -3,12 +3,11 @@ import { Component, lazy, Suspense } from 'solid-js';
 import AppFooter from './AppFooter';
 import AppHeader from './AppHeader';
 import { getIsShowRootActions, getOverlayElement } from './store/root';
-import ActionPage from './views/action/ActionPage';
 import CollectionPage from './views/collection/CollectionPage';
 import LoadingPage from './views/loading/LoadingPage';
 
 const MusclePage = lazy(() => import('./views/muscle/MusclePage'));
-// const ActionPage = lazy(() => import('./views/action/ActionPage'));
+const ActionPage = lazy(() => import('./views/action/ActionPage'));
 const ResultPage = lazy(() => import('./views/result/ResultPage'));
 const SettingsPage = lazy(() => import('./views/settings/SettingsPage'));
 
